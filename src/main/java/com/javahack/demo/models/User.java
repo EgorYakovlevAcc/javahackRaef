94de5fcfb99b44f7b64cfd6ffa9d154c2fbdd75f
Egor Yakovlev <yakovlev.es@phystech.edu>
2019-09-05 00:06:24 +0300

    id and generate type added to User

--- src/main/java/com/javahack/demo/models/User.java
+++ src/main/java/com/javahack/demo/models/User.java
@@ -4,10 +4,15 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.util.Collection;
 
 @Entity
 public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
     private String login;
     private String password;
