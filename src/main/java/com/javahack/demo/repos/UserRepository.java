63f80bf71bd4005f5af058e4f39e2f40a9ef9a2c
Egor Yakovlev <yakovlev.es@phystech.edu>
2019-09-09 19:03:16 +0300

    user details update

--- src/main/java/com/javahack/demo/repos/UserRepository.java
+++ src/main/java/com/javahack/demo/repos/UserRepository.java
@@ -7,4 +7,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
     User findUserById(Integer id);
+    User findUserByLogin(String login);
 }
---
+++ src/main/java/com/javahack/demo/services/userdetails/CustomUserDetailsService.java
@@ -0,0 +1,18 @@
+package com.javahack.demo.services.userdetails;
+
+import com.javahack.demo.repos.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    @Autowired
+    UserRepository userRepository;
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        return userRepository.findUserByLogin(s);
+    }
+}
